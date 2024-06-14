faf4576313531281ab0dbed1e977e2f800a86013
RutujChordiya <57297289+RutujChordiya@users.noreply.github.com>
2024-06-14 13:42:09 +0530

    Update main.py
    Font size update.

--- Basic python-flask webapp/main.py
+++ Basic python-flask webapp/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello, World!</h1><p><h2>My name is Rutuj Kiran Chordiya <br> </h2> </p> <p>This is a basic Flask web application that I've created to test out various Devops tools and practices.</p>"
+    return "<h1>Hello, World!</h1><p><h2>My name is Rutuj Kiran Chordiya <br> </h2> </p> <p> <h4> This is a basic Flask web application that I've created to test out various Devops tools and practices.</h4> </p>"
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
