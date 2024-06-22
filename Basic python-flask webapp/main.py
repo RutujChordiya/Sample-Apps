6fe644dee9adb3cc034a4a30d993ac3eedd44ab9
RutujChordiya <rutujchordiya@gmail.com>
2024-06-22 10:45:59 +0530

    Updated project

--- Basic python-flask webapp/main.py
+++ Basic python-flask webapp/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello, World!</h1><p><h2>My name is Rutuj Kiran Chordiya <br> </h2> </p> <p> <h4> This is a basic Flask web application that I've created to test out various Devops tools and practices like jenkins.</h4> </p>"
+    return "<h1>Hello, World!</h1><p>My name is Rutuj Kiran Chordiya and this is a basic Flask web application that I've created to test out various Devops tools and practices using AWS services only.</p>"
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True)
---
+++ Basic python-flask webapp/scripts/stop_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Stop the Flask application
+sudo pkill -f "python /var/www/flask/main.py"
