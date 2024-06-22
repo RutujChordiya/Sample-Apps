615f7b16043a1468838aef5242ab9cb068cdd415
RutujChordiya <rutujchordiya@gmail.com>
2024-06-22 20:01:14 +0530

    File structure reformatted

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Hello, World!</h1><p>My name is Rutuj Kiran Chordiya and this is a basic Flask web application that I've created to test out various Devops tools and practices using AWS services only.</p>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
